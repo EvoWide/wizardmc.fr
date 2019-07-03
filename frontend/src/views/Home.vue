3a78fdbdf736a67f2026fdf8b4d91a683eaa82ac
Valentin Kaelin <kalanehd@gmail.com>
2019-07-03 21:24:12 +0200

    Vue cli installation with TailwindCSS and PurgeCSS

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="text-purple-500 text-2xl text-semibold">Hello World!</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
