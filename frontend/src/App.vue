a7df7c3fb997fb2817ff6eb6d2c040a026739e50
Valentin Kaelin <kalanehd@gmail.com>
2019-07-06 16:13:00 +0200

    Vue component for navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,16 @@
 <template>
   <div id="app" class="bg-gray-200 min-h-screen">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-      <router-link to="/login">Connexion</router-link>|
-      <router-link to="/register">Inscription</router-link>|
-      <button v-if="isAuthenticated" @click="logout" type="button">Déconnexion</button>
-    </div>
+    <navigation />
     <router-view class="container mx-auto" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { AUTH_LOGOUT } from '@/store/actions/auth'
+import Navigation from '@/components/Navigation'
 
 export default {
-  methods: {
-    logout () {
-      this.$store.dispatch(AUTH_LOGOUT)
-        .then(() => {
-          this.$router.push('/login')
-        })
-    }
-  },
-
-  computed: {
-    ...mapGetters(['isAuthenticated'])
+  components: {
+    Navigation
   },
 
   created () {
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>|
+    <router-link to="/login">Connexion</router-link>|
+    <router-link to="/register">Inscription</router-link>|
+    <button v-if="isAuthenticated" @click="logout" type="button">Déconnexion</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { AUTH_LOGOUT } from '@/store/actions/auth'
+
+export default {
+  methods: {
+    logout () {
+      this.$store.dispatch(AUTH_LOGOUT)
+        .then(() => {
+          this.$router.push('/login')
+        })
+    }
+  },
+
+  computed: {
+    ...mapGetters(['isAuthenticated'])
+  }
+}
+</script>
