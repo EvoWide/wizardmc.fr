f7af003941526ff837a3beda44ecfffdadc9bffe
Valentin Kaelin <kalanehd@gmail.com>
2020-04-19 14:30:14 +0200

    chore: add favicon

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -5,11 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'WizardMC',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'WizardMC est serveur factions minecraft sous launcher.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
