eac02cdf24b1802c78bbde508c50058676013e5b
Forsties08 <forsties08@gmail.com>
2020-05-08 13:32:42 +0200

    feat: shop view action

--- api/app/Controllers/Http/ShopController.ts
+++ api/app/Controllers/Http/ShopController.ts
@@ -1,12 +1,22 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Category from 'App/Models/Shop/Category'
+import Offer from 'App/Models/Shop/Offer'
 
 export default class ShopsController {
   public async index ({ response }: HttpContextContract) {
     const offers = await Category.query().select('id', 'name').preload('offers', (builder) => {
-      builder.select('id', 'name', 'description', 'image', 'price')
+      builder.select('id', 'name', 'image', 'price')
     })
 
     response.send(offers)
+  }
+
+  public async view ({ response, params }: HttpContextContract) {
+    const offer = await Offer.query()
+      .where('id', params.id)
+      .select('id', 'name', 'image', 'description', 'price')
+      .firstOrFail()
+
+    response.send(offer)
   }
 }
