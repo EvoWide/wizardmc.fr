61626e65de79032435469c2a27415fe42fc0dfa8
Forsties08 <forsties08@gmail.com>
2020-05-10 16:18:21 +0200

    fix: temporary fix for the config

--- api/app/Services/RecaptchaService.ts
+++ api/app/Services/RecaptchaService.ts
@@ -1,15 +1,15 @@
 import got from 'got'
-import wizardConfig from 'config/wizard'
+import Env from '@ioc:Adonis/Core/Env'
 
 class Recaptcha {
   public async verify (token: string) {
     const response = await got.post('https://www.google.com/recaptcha/api/siteverify', {
+      responseType: 'json',
       json: {
-        secret: wizardConfig.recaptcha.privateKey,
+        secret: Env.get('RECAPTCHA_PRIVATE_KEY'),
         response: token,
-      }, responseType: 'json',
+      },
     })
-
     return response.statusCode === 200
   }
 }
