24e60d340ffdf18acf1305ffe2ab915a6f721e88
Valentin Kaelin <kalanehd@gmail.com>
2020-05-11 19:16:00 +0200

    feat: add some ugly tmp error page

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container py-24 mx-auto text-center text-white">
+    <div class="text-xl font-semibold uppercase">
+      <h1 v-if="error.statusCode === 404">Page non trouvée</h1>
+      <h1 v-else>Une erreur s'est produite.</h1>
+    </div>
+    <nuxt-link
+      to="/"
+      class="inline-flex px-5 py-4 mt-16 text-xl font-bold text-yellow-600 uppercase border-2 btn-cta bg-gradient border-gradient font-title"
+    >Retour à l'accueil</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
