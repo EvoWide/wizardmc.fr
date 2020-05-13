a9cff7386aadd955819549dbf2454ce2a227ef90
Forsties08 <forsties08@gmail.com>
2020-05-13 19:05:50 +0200

    fix: dirty fix

--- api/server.ts
+++ api/server.ts
@@ -15,6 +15,7 @@ import sourceMapSupport from 'source-map-support'
 import { Ignitor } from '@adonisjs/core/build/src/Ignitor'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
+process.env.mainApp = 'true'
 
 new Ignitor(__dirname)
   .httpServer()
