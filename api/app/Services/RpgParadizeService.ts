44b5ccc8d83e15d6c92ca9c5b660fea943cc6123
Forsties08 <forsties08@gmail.com>
2020-05-13 19:05:26 +0200

    feat: new rpgparadize service

---
+++ api/app/Services/RpgParadizeService.ts
@@ -0,0 +1,26 @@
+import { JSDOM } from 'jsdom'
+import Env from '@ioc:Adonis/Core/Env'
+
+class RpgParadizeService {
+  private out = -1
+
+  public getOut () {
+    return this.out
+  }
+
+  public async update () {
+    const url = `http://www.rpg-paradize.com/site-my-favorite-minecraft-server-${Env.get('RPG_PARADIZE_ID')}`
+    try {
+      const dom = await JSDOM.fromURL(url)
+      let line: string = dom.window.document
+        .querySelector('body > div.div-content > div.div-page > div:nth-child(1) > div:nth-child(7)')
+        .lastChild.textContent
+      const result = Number(line.split(' : ')[1].trim())
+      this.out = isNaN(result) ? -1 : result
+    } catch {
+
+    }
+  }
+}
+
+export default new RpgParadizeService()
