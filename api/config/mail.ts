d9216b998062d9abd78d750b1c939788be4fdacf
Valentin Kaelin <kalanehd@gmail.com>
2020-05-17 19:56:14 +0200

    fix: rename smtp variable env

--- api/config/mail.ts
+++ api/config/mail.ts
@@ -37,11 +37,11 @@ const mailConfig: MailerConfigContract = {
     smtp: {
       driver: 'smtp',
       host: Env.get('SMTP_HOST', '127.0.0.1') as string,
-      port: Number(Env.get('SMTP_PORT', '587')),
+      port: Number(Env.get('SMTP_PORT', 587)),
       auth: {
         type: 'login',
         user: Env.get('SMTP_USER', 'user') as string,
-        pass: Env.get('SMTP_PASS', 'pass') as string,
+        pass: Env.get('SMTP_PASSWORD', 'password') as string,
       },
     },
   },
