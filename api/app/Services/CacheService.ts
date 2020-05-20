b2d4bdd30957685ada8c01ed672a9c71f0cf40b5
Forsties08 <forsties08@gmail.com>
2020-05-20 14:15:19 +0200

    feat: cache system with expiration

---
+++ api/app/Services/CacheService.ts
@@ -0,0 +1,67 @@
+import { Duration } from 'luxon'
+import ms from 'ms'
+
+class CacheService {
+  private cache = new Map<string, CacheValue>()
+
+  public put (key: string, value: any, duration?: Duration | string) {
+    if (!duration) {
+      this.cache.set(key, { data: value })
+      return
+    }
+
+    let expireAt: number
+    if (typeof (duration) === 'string') {
+      expireAt = ms(duration)
+    } else {
+      expireAt = duration.as('millisecond')
+    }
+
+    const data: ExpiringCacheValue = {
+      data: value,
+      expireAt: new Date().getTime() + expireAt,
+    }
+    this.cache.set(key, data)
+  }
+
+  public remove (key: string): boolean {
+    return this.cache.delete(key)
+  }
+
+  public async remember (key: string, method: () => any, duration: Duration | string) {
+    const currentValue = this.get(key)
+    if (currentValue) {
+      return currentValue.data
+    }
+
+    const data = method()
+
+    this.put(key, data, duration)
+    return data
+  }
+
+  public get (key: string, defValue?: any): any {
+    const currentValue = this.cache.get(key)
+    if (!currentValue || this.isExpired(currentValue)) {
+      return defValue ?? null
+    }
+    return currentValue
+  }
+
+  private isExpired (value: CacheValue) {
+    if ('expireAt' in value) {
+      return (value as ExpiringCacheValue).expireAt <= new Date().getTime()
+    }
+    return false
+  }
+}
+
+interface CacheValue {
+  data: any
+}
+
+interface ExpiringCacheValue extends CacheValue {
+  expireAt: number
+}
+
+export default new CacheService()
