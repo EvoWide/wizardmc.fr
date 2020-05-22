6fdcd312245f69c89a84c1d50998b1adfdfcf659
Valentin Kaelin <kalanehd@gmail.com>
2020-05-22 15:15:07 +0200

    fix: profile history check

--- api/app/Controllers/Http/Profile/HistoriesController.ts
+++ api/app/Controllers/Http/Profile/HistoriesController.ts
@@ -3,7 +3,7 @@ import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class HistoriesController {
   public async index ({ response, auth, params, session }: HttpContextContract) {
-    if (['shop'].includes(params.type)) {
+    if (!['shop'].includes(params.type)) {
       return response.globalError('La page demandée n\'a pas été trouvée', 404)
     }
 
