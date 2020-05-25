96ef59bc172b8012cc1add0838a0d69ed457f6ca
Forsties08 <forsties08@gmail.com>
2020-05-25 17:44:09 +0200

    feat: paypal service for ipn

---
+++ api/app/Services/Payment/Paypal.ts
@@ -0,0 +1,50 @@
+import Env from '@ioc:Adonis/Core/Env'
+import got from 'got'
+import qs from 'querystring'
+
+class Paypal {
+  private readonly url: string = 'https://paypal.com/cgi-bin/webscr'
+  private readonly sandbox_url: string = 'https://sandbox.paypal.com/cgi-bin/webscr'
+
+  constructor (
+    protected sandbox = Env.get('PAYMENT_PAYPAL_SANDBOX'),
+    protected email = Env.get('PAYMENT_PAYPAL_EMAIL'),
+  ) { }
+
+  public async verify (payload: any): Promise<boolean> {
+    payload.cmd = '_notify-validate'
+    let response
+
+    try {
+      response = await got.post(this.getUrl(), {
+        body: qs.stringify(payload),
+      })
+    } catch (ex) {
+      return false
+    }
+
+    if (response.body !== 'VERIFIED') {
+      return false
+    }
+
+    if (payload.payment_status !== 'Completed') {
+      return false
+    }
+
+    if (payload.receiver_email !== this.email) {
+      return false
+    }
+
+    if (!payload.custom || isNaN(payload.custom)) {
+      return false
+    }
+
+    return true
+  }
+
+  private getUrl (): string {
+    return this.sandbox ? this.sandbox_url : this.url
+  }
+}
+
+export default new Paypal() as Paypal
