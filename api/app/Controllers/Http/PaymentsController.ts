7bfe76c19c785f3f27947f999dceaa982ef70ffe
Forsties08 <forsties08@gmail.com>
2020-05-25 23:41:33 +0200

    feat: dedipass fully fonctionnal

--- api/app/Controllers/Http/PaymentsController.ts
+++ api/app/Controllers/Http/PaymentsController.ts
@@ -1,19 +1,50 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import CacheService from 'App/Services/CacheService'
 import Dedipass from 'App/Services/Payment/Dedipass'
 import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class PaymentsController {
-  public async rates () {
+  public async rates ({ response }: HttpContextContract) {
     const dedipassRates = await CacheService.remember('dedipass-rates', async () => {
       return await Dedipass.getRates()
     }, '1h')
 
-    return {
+    return response.send({
       dedipass: dedipassRates,
       paypal: await Database.from('payment_prices')
         .where('method', 'paypal')
         .select('price', 'credits'),
+    })
+  }
+
+  public async dedipass ({ request, auth, response }: HttpContextContract) {
+    const { code } = request.post()
+    if (!code || !code.math(/^[a-z0-9]+$/i)) {
+      return response.globalError('Le code est dans un format incorrect.')
     }
+
+    const dedipassValidation = await Dedipass.validate(code)
+    if (!dedipassValidation || dedipassValidation.status !== 'success') {
+      return response.globalError('Le code entré est incorrect.')
+    }
+
+    const credits = dedipassValidation.virtual_currency ?? 0
+    if (credits) {
+      auth.user!.credits += credits
+      await auth.user!.save
+    }
+
+    await Database
+      .insertQuery()
+      .table('user_payments')
+      .insert({
+        user_id: auth.user!.id,
+        method: 'dedipass',
+        price: await Dedipass.getUserPrice(credits),
+        credits: credits,
+        data: JSON.stringify({ code: code }),
+      })
+
+    return response.globalSuccess(`Votre compte a bien été débité de ${credits} !`)
   }
 }
