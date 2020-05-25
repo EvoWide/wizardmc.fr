7003e2492f6425cc84055122887a2e3ee5b4ef4f
Forsties08 <forsties08@gmail.com>
2020-05-25 17:43:43 +0200

    feat: payment prices (paypal / dedipass only)

---
+++ api/database/migrations/1590417731577_payment_prices.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class PaymentPrices extends BaseSchema {
+  protected tableName = 'payment_prices'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('method')
+      table.decimal('price')
+      table.integer('credits')
+      table.timestamp('created_at').defaultTo(this.now())
+      table.timestamp('updated_at').defaultTo(this.now())
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
