5e7ad0d5f56628468ddb42ca9f8a8c7da397ae32
Valentin Kaelin <kalanehd@gmail.com>
2020-05-26 13:33:19 +0200

    feat: display dedipass offers to users

--- frontend/components/Shop/Credits/Dedipass.vue
+++ frontend/components/Shop/Credits/Dedipass.vue
@@ -4,12 +4,21 @@
       <span class="text-purple-200">Région</span>
       <select v-model="region" class="block w-full mt-1 form-select">
         <option
-          v-for="offer in Object.keys(choosableRates)"
+          v-for="offer in Object.keys(choosableRegions)"
           :key="offer"
           :value="offer"
         >{{ rates[offer].name }}</option>
       </select>
     </label>
+    <div class="mt-4">
+      <div v-for="[name, offers] in selectedRegionMethods" :key="name">
+        <div class="mt-2 text-lg">{{ name }}</div>
+        <div
+          v-for="offer in offers"
+          :key="offer.rate"
+        >{{ offer.user_earns }}PB - {{ offer.user_price }} {{ offer.user_currency }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,10 +38,20 @@ export default {
   },
 
   computed: {
-    choosableRates () {
+    choosableRegions () {
       const rates = { ...this.rates }
       delete rates[0]
       return rates
+    },
+    selectedRegion () {
+      return this.rates[this.region]
+    },
+    selectedRegionMethods () {
+      const methods = Object.entries({ ...this.selectedRegion.methods, ...this.rates[0].methods })
+      console.log('-----------------')
+      console.log(methods)
+
+      return methods
     }
   }
 }
