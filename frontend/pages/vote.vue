977389ae5fc182e6ecc1c58149aa67de295d543b
Valentin Kaelin <kalanehd@gmail.com>
2020-06-02 13:31:21 +0200

    feat: add disclaimer on vote page

--- frontend/pages/vote.vue
+++ frontend/pages/vote.vue
@@ -57,6 +57,25 @@
           <div v-if="logged && lastVote === ''" class="px-4">
             <template v-if="currentStep === 0">
               <div class="text-center">
+                <!-- TODO: Remove it before server opens -->
+                <div
+                  class="flex items-center px-4 py-2 mb-4 text-sm text-justify text-yellow-500 bg-purple-800 border border-red-600 rounded-md md:px-6 md:py-4"
+                >
+                  <svg
+                    class="flex-shrink-0 w-6 h-6 text-red-600"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                  <p
+                    class="ml-3 md:ml-5"
+                  >Les récompenses de vote en attente seront remises à zéro avant l'ouverture du serveur pour éviter une différence de stuff trop importante.</p>
+                </div>
                 <div
                   class="flex items-center px-4 py-2 text-sm text-justify bg-purple-800 rounded-md md:px-6 md:py-4"
                 >
