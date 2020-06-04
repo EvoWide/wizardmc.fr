55ad8c6604559aa2621bb29d07ece7e04d2bad41
Valentin Kaelin <kalanehd@gmail.com>
2020-06-04 16:06:41 +0200

    fix: add current working directory in nodejs exec based on env var

--- api/app/Controllers/Http/Admin/CommandsController.ts
+++ api/app/Controllers/Http/Admin/CommandsController.ts
@@ -2,16 +2,18 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 const util = require('util')
 // const exec = util.promisify(require('child_process').exec)
 const execFile = util.promisify(require('child_process').execFile)
+import Env from '@ioc:Adonis/Core/Env'
 
 export default class CommandsController {
   public async deploy ({ params, response }: HttpContextContract) {
     const files = {
-      'frontend': '../../../deploy-wizardmc.sh',
-      'api': '../../../deploy-api.wizardmc.sh',
-      'admin': '../../../deploy-admin.wizardmc.sh',
+      'frontend': 'deploy-wizardmc.sh',
+      'api': 'deploy-api.wizardmc.sh',
+      'admin': 'deploy-admin.wizardmc.sh',
     }
-
     const file = files[params.app]
+    const scriptsFolder = Env.get('SCRIPTS_FOLDER') as string
+    console.log(scriptsFolder)
 
     if (!file) {
       return response.globalError('App spécifiée non existante.')
@@ -20,10 +22,10 @@ export default class CommandsController {
     async function getVersion () {
       // const { stdout } = await execFile('node', ['--version'])
       // const { stdout } = await execFile(file)
-      const { stdout } = await execFile('bash', [file])
+      const { stdout } = await execFile('bash', [file], { cwd: scriptsFolder })
       // const { stdout } = await exec('cd')
       return stdout
     }
-    return response.json({output: await getVersion() })
+    return response.json({ output: await getVersion() })
   }
 }
