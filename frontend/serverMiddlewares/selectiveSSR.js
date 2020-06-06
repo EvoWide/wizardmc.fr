1c07697bb5d10420fd5c680fa8c0415615ce86ba
Valentin Kaelin <kalanehd@gmail.com>
2020-06-06 17:45:25 +0200

    fix: nuxt server middleware checks

--- frontend/serverMiddlewares/selectiveSSR.js
+++ frontend/serverMiddlewares/selectiveSSR.js
@@ -1,10 +1,12 @@
 export default function (req, res, next) {
   const paths = ['/users/profile', '/credits', '/users/security']
 
-  if (paths.includes(req.originalUrl)) {
-    // Will trigger the "traditional SPA mode"
-    res.spa = true
-  }
+  paths.map((p) => {
+    if (req.originalUrl.includes(p)) {
+      // Will trigger the "traditional SPA mode"
+      res.spa = true
+    }
+  })
 
   next()
 }
