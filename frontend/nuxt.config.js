bc09b5c4a0a4a739cf1fcb48c672aaa8f1f1da8a
Valentin Kaelin <kalanehd@gmail.com>
2020-06-08 05:12:14 +0200

    feat: add favicon for all plateforms

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -15,10 +15,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'WizardMC est serveur factions minecraft sous launcher.' }
+      { hid: 'description', name: 'description', content: 'WizardMC est serveur factions minecraft sous launcher.' },
+      { name: 'msapplication-TileColor', content: '#44337a' },
+      { name: 'theme-color', content: '#44337a' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#44337a' },
       { rel: 'preconnect', href: 'https://wizardmc.fr' },
       { rel: 'preconnect', href: 'https://api.wizardmc.fr' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
