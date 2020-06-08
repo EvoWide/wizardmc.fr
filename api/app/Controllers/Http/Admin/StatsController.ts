b7e83907a083920d5385d4296e2b9b1989b4c21c
Valentin Kaelin <kalanehd@gmail.com>
2020-06-08 11:10:57 +0200

    Revert "fix: replace raw query by query builder for registrations"
    This reverts commit 13c94b1b9abfa732be6e3e4e58d2ddcb55a69c88.

--- api/app/Controllers/Http/Admin/StatsController.ts
+++ api/app/Controllers/Http/Admin/StatsController.ts
@@ -19,13 +19,12 @@ export default class StatsController {
   }
 
   public async registrations ({ response }: HttpContextContract) {
-    const registrations = await Database.from('users')
-      .select('created_at')
-      .count('id as count')
-      .groupByRaw('DATE(created_at)')
-      .orderBy('created_at', 'asc')
+    const registrations = await Database.rawQuery(`
+      SELECT DATE(created_at) as registerdate, COUNT(id) as count
+      FROM Users GROUP BY registerdate ORDER BY registerdate ASC
+    `)
 
-    return response.json(registrations)
+    return response.json(registrations.rows ?? registrations)
   }
 
   public async players ({ response }: HttpContextContract) {
