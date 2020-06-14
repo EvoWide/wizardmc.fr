1df987717a4c8ea8e089390e7307fe9bf87da15e
Forsties08 <forsties08@gmail.com>
2020-06-14 21:18:04 +0200

    refactor: use correct validator in rest api

--- api/app/Controllers/Http/Rest/UsersController.ts
+++ api/app/Controllers/Http/Rest/UsersController.ts
@@ -1,13 +1,15 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 export default class UsersController {
   // Fields: player
   public async balance ({ request, response }: HttpContextContract) {
-    const { player } = request.post()
-    if (!player) {
-      return response.status(400).json({ message: 'Invalid request' })
-    }
+    const { player } = await request.validate({
+      schema: schema.create({
+        player: schema.string(),
+      }),
+    })
 
     const user = await this.findByIdOrUUID(player)
     return response.send({ success: true, balance: user.credits })
@@ -15,10 +17,12 @@ export default class UsersController {
 
   // Fields player, credits
   public async withdraw ({ request, response }: HttpContextContract) {
-    const { player, credits } = request.post()
-    if (!player || !credits || isNaN(credits)) {
-      return response.status(400).json({ message: 'Invalid request' })
-    }
+    const { player, credits } = await request.validate({
+      schema: schema.create({
+        player: schema.string(),
+        credits: schema.number(),
+      }),
+    })
 
     const user = await this.findByIdOrUUID(player)
     if (user.credits < credits) {
@@ -33,10 +37,12 @@ export default class UsersController {
 
   // Fields player, credits
   public async deposit ({ request, response }: HttpContextContract) {
-    const { player, credits } = request.post()
-    if (!player || !credits || isNaN(credits)) {
-      return response.status(400).json({ message: 'Invalid request' })
-    }
+    const { player, credits } = await request.validate({
+      schema: schema.create({
+        player: schema.string(),
+        credits: schema.number(),
+      }),
+    })
 
     const user = await this.findByIdOrUUID(player)
     user.credits += credits
@@ -47,10 +53,13 @@ export default class UsersController {
 
   // Fields: player, target, credits
   public async transfer ({ request, response }: HttpContextContract) {
-    const { player, target, credits } = request.post()
-    if (!player || !target || !credits || isNaN(credits)) {
-      return response.status(400).json({ message: 'Invalid request' })
-    }
+    const { player, target, credits } = await request.validate({
+      schema: schema.create({
+        player: schema.string(),
+        target: schema.string(),
+        credits: schema.number(),
+      }),
+    })
 
     const user = await this.findByIdOrUUID(player)
     const userTarget = await this.findByIdOrUUID(target)
